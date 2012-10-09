6e75654ede2d326a26c9ab830e9fafc05ccf8e87
Jan Halfar <jan@bestbytes.com>
2012-10-09 14:13:33 +0200

    improved tests for reflection

--- tests/Foomo/Reflection/PhpDocEntryTest.php
+++ tests/Foomo/Reflection/PhpDocEntryTest.php
@@ -25,77 +25,90 @@ namespace Foomo\Reflection;
  * @author jan <jan@bestbytes.de>
  */
 class PhpDocEntryTest extends \PHPUnit_Framework_TestCase {
+	const MOCK_NAMESPACE = 'Foomo\\Reflection';
 	const MOCK_CLASS_NAME = 'Foomo\\Reflection\\MockClass';
 	public function testClassProps()
 	{
 		$classRefl = new \ReflectionClass(self::MOCK_CLASS_NAME);
-		$classDoc = new PhpDocEntry($classRefl->getDocComment());
+		$namespaces = array('', $classRefl->getNamespaceName());
+		foreach($namespaces as $namespace) {
+			$classDoc = new PhpDocEntry($classRefl->getDocComment(), $namespace);
 
-		$seppRead = $this->getPropDocs('seppRead', $classDoc);
-		$this->assertTrue($seppRead->read, 'sepp read must be readable');
-		$this->assertFalse($seppRead->write, 'sepp read must not be writable');
+			$seppRead = $this->getPropDocs('seppRead', $classDoc);
+			$this->assertTrue($seppRead->read, 'sepp read must be readable');
+			$this->assertFalse($seppRead->write, 'sepp read must not be writable');
 
-		$seppWrite = $this->getPropDocs('seppWrite', $classDoc);
-		$this->assertFalse($seppWrite->read, 'seppWrite must not be readable');
-		$this->assertTrue($seppWrite->write, 'seppWrite must be writable');
+			$seppWrite = $this->getPropDocs('seppWrite', $classDoc);
+			$this->assertFalse($seppWrite->read, 'seppWrite must not be readable');
+			$this->assertTrue($seppWrite->write, 'seppWrite must be writable');
 
-		$seppReadWrite = $this->getPropDocs('seppReadWrite', $classDoc);
-		$this->assertTrue($seppReadWrite->write && $seppReadWrite->read, 'must be readable and writable');
-
+			$seppReadWrite = $this->getPropDocs('seppReadWrite', $classDoc);
+			$this->assertTrue($seppReadWrite->write && $seppReadWrite->read, 'must be readable and writable');
+		}
 	}
 
 	public function testParameters()
 	{
-		$docEntry = $this->getTestMethodDocEntry();
-		$argBar = $this->getArg('bar', $docEntry);
-		$argFooBar = $this->getArg('fooBar', $docEntry);
+		$namespaces = array('', self::MOCK_NAMESPACE);
+		foreach($namespaces as $namespace) {
+			$docEntry = $this->getTestMethodDocEntry($namespace);
+			$argBar = $this->getArg('bar', $docEntry);
+			$argFooBar = $this->getArg('fooBar', $docEntry);
 
-		$this->assertEquals($argBar->name, 'bar');
-		$this->assertEquals($argBar->type, 'string');
-		$this->assertEquals($argBar->comment, 'bar bar bar');
+			$this->assertEquals($argBar->name, 'bar');
+			$this->assertEquals($argBar->type, 'string');
+			$this->assertEquals($argBar->comment, 'bar bar bar');
 
-		$this->assertEquals($argFooBar->name, 'fooBar');
-		$this->assertEquals($argFooBar->type, 'array');
-		$this->assertEquals($argFooBar->comment, 'foo bar comment');
+			$this->assertEquals($argFooBar->name, 'fooBar');
+			$this->assertEquals($argFooBar->type, 'array');
+			$this->assertEquals($argFooBar->comment, 'foo bar comment');
+		}
 	}
 	/**
 	 * test method doc entry
 	 *
 	 * @return PhpDocEntry
 	 */
-	private function getTestMethodDocEntry()
+	private function getTestMethodDocEntry($namespace)
 	{
 		$methodRefl = new \ReflectionMethod(self::MOCK_CLASS_NAME, 'foo');
-		return new PhpDocEntry($methodRefl->getDocComment());
+		return new PhpDocEntry($methodRefl->getDocComment(), $namespace);
 	}
 	public function testMiscMethodDocs()
 	{
-
-		$docEntry = $this->getTestMethodDocEntry();
-		$this->assertEquals('ignore', $docEntry->serviceGen);
-		$this->assertEquals('ignore', $docEntry->wsdlGen);
-		$this->assertEquals('somewhere else', $docEntry->see);
-		$this->assertEquals('string', $docEntry->return->type);
-		$this->assertEquals('well it returns a poem', $docEntry->return->comment);
-		$this->assertEquals('MyMessage', $docEntry->serviceMessage[0]->type);
-		$this->assertEquals('jan', $docEntry->author);
+		$namespaces = array('', self::MOCK_NAMESPACE);
+		foreach($namespaces as $namespace) {
+			$docEntry = $this->getTestMethodDocEntry($namespace);
+			$this->assertEquals('ignore', $docEntry->serviceGen);
+			$this->assertEquals('ignore', $docEntry->wsdlGen);
+			$this->assertEquals('somewhere else', $docEntry->see);
+			$this->assertEquals('string', $docEntry->return->type);
+			$this->assertEquals('well it returns a poem', $docEntry->return->comment);
+			$this->assertEquals('MyMessage', $docEntry->serviceMessage[0]->type);
+			$this->assertEquals('jan', $docEntry->author);
+		}
 	}
 
 	public function testClassProp()
 	{
 		$refl = new \ReflectionProperty(self::MOCK_CLASS_NAME, 'foo');
-		$docEntry = new PhpDocEntry($refl->getDocComment());
-		$this->assertEquals($docEntry->comment, 'foo prop');
-		$this->assertEquals($docEntry->var->type, self::MOCK_CLASS_NAME);
+		$namespaces = array('', self::MOCK_NAMESPACE);
+		foreach($namespaces as $namespace) {
+			$docEntry = new PhpDocEntry($refl->getDocComment(), $namespace);
+			$this->assertEquals($docEntry->comment, 'foo prop');
+			$this->assertEquals($docEntry->var->type, self::MOCK_CLASS_NAME);
+		}
 	}
 	public function testResolveType()
 	{
-		$entry = new PhpDocEntry(null, __NAMESPACE__);
-		$this->assertEquals(__NAMESPACE__ . '\\PhpDocEntryTest', $entry->resolveType(__CLASS__), __NAMESPACE__ . '\\PhpDocEntryTest');
-		$this->assertEquals(__NAMESPACE__ . '\\PhpDocEntryTest[]', $res = $entry->resolveType(__CLASS__ .'[]'), __NAMESPACE__ . '\\PhpDocEntryTest[] != ' . $res);
-		$this->assertEquals('Exception', $entry->resolveType('\\Exception'), 'Exception');
-		$this->assertEquals('Exception[]', $entry->resolveType('\\Exception[]'), 'Exception[]');
-		$this->assertEquals('Foomo\\Reflection\\MockClass',	$entry->resolveType('Foomo\\Reflection\\MockClass'), 'Foomo\\Reflection\\MockClass');
+		foreach(array('', __NAMESPACE__) as $namespace) {
+			$entry = new PhpDocEntry(null, $namespace);
+			$this->assertEquals(__NAMESPACE__ . '\\PhpDocEntryTest', $entry->resolveType(__CLASS__), __NAMESPACE__ . '\\PhpDocEntryTest');
+			$this->assertEquals(__NAMESPACE__ . '\\PhpDocEntryTest[]', $res = $entry->resolveType(__CLASS__ .'[]'), __NAMESPACE__ . '\\PhpDocEntryTest[] != ' . $res);
+			$this->assertEquals('Exception', $entry->resolveType('\\Exception'), 'Exception');
+			$this->assertEquals('Exception[]', $entry->resolveType('\\Exception[]'), 'Exception[]');
+			$this->assertEquals('Foomo\\Reflection\\MockClass',	$entry->resolveType('Foomo\\Reflection\\MockClass'), 'Foomo\\Reflection\\MockClass');
+		}
 	}
 	/**
 	 * @param type $name
@@ -105,7 +118,6 @@ class PhpDocEntryTest extends \PHPUnit_Framework_TestCase {
 	private function getArg($name, PhpDocEntry $docEntry)
 	{
 		foreach($docEntry->parameters as $parameter) {
-
 			if($parameter->name == $name) {
 				return $parameter;
 			}
