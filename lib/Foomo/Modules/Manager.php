5bafdd66b31722f89d3e6573f0907444d5e20f92
Bostjan Marusic <bostjan@bestbytes.de>
2013-07-22 10:51:53 +0200

    fixed doc comment

--- lib/Foomo/Modules/Manager.php
+++ lib/Foomo/Modules/Manager.php
@@ -871,7 +871,7 @@ class Manager
 	 *
 	 * @param string[] $targets
 	 *
-	 * @return MakeResult[]
+	 * @return array target => MakeResult[], target => MakeResult[], ...
 	 */
 	public static function make($targets)
 	{
