79230ed5ac7af93bb5fbe45ce4eaf914857ffaf1
Jan Halfar <jan@bestbytes.com>
2013-12-29 01:02:09 +0100

    MVC cleanup in the log frontend

--- lib/Foomo/Log/Frontend/Model.php
+++ lib/Foomo/Log/Frontend/Model.php
@@ -28,23 +28,4 @@ use Foomo\AutoLoader;
  * @author jan <jan@bestbytes.de>
  */
 class Model {
-	public function webTail($filters)
-	{
-		header('Content-Type: text/plain;charset=utf-8;');
-
-		echo __METHOD__ . ':' . PHP_EOL . PHP_EOL . implode(PHP_EOL, $filters) . PHP_EOL . PHP_EOL;
-
-		$filter = function (\Foomo\Log\Entry $entry) use ($filters) {
-					foreach ($filters as $filter) {
-						$filter = explode('::', $filter);
-						if (!\call_user_func_array($filter, array($entry))) {
-							return false;
-						}
-					}
-					return true;
-				};
-
-		$utils = new \Foomo\Log\Utils();
-		$utils->webTail(\Foomo\Log\Logger::getLoggerFile(), $filter);
-	}
 }
