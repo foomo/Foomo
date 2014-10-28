161ae4d2579c5fe17513df5a28e60f4a0fcbe1ea
Frederik Löffert <frederik@zitrusmedia.de>
2014-10-28 20:56:44 +0100

    composer phar updated and improved composer error logging

--- lib/Foomo/Composer.php
+++ lib/Foomo/Composer.php
@@ -18,7 +18,6 @@
  */
 
 namespace Foomo;
-use Foomo\Modules\Manager;
 
 /**
  * composer integration
@@ -36,6 +35,9 @@ class Composer
 	 */
 	private static function callComposer()
 	{
+		if(!CORE_CONFIG_DIR_COMPOSER) {
+			trigger_error('composer directory ' . dirname(dirname(\Foomo\ROOT)) . '/composer does not exist', E_USER_WARNING);
+		}
 		$args = array_merge(
 			array(
 				Module::getBaseDir('bin') . DIRECTORY_SEPARATOR . 'composer.phar',
