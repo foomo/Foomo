cbbf89e5418cacf9537bcbb3a4d4eff1dd200b5a
Jan Halfar <jan@bestbytes.com>
2016-02-26 16:22:35 +0100

    bumped version 0.4.2 => 0.4.3

--- lib/Foomo/Module.php
+++ lib/Foomo/Module.php
@@ -37,7 +37,7 @@ class Module extends \Foomo\Modules\ModuleBase implements \Foomo\Frontend\Toolbo
 	//---------------------------------------------------------------------------------------------
 
 	const NAME = 'Foomo';
-	const VERSION = '0.4.2';
+	const VERSION = '0.4.3';
 
 	//---------------------------------------------------------------------------------------------
 	// ~ Overriden static methods
