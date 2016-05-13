76d98b0126489ec379ca24c1a975493d12e2ed1c
Uwe Quitter <uwe.quitter@bestbytes.de>
2016-05-13 14:29:06 +0200

    module version++ (0.4.11)

--- lib/Foomo/Module.php
+++ lib/Foomo/Module.php
@@ -37,7 +37,7 @@ class Module extends \Foomo\Modules\ModuleBase implements \Foomo\Frontend\Toolbo
 	//---------------------------------------------------------------------------------------------
 
 	const NAME = 'Foomo';
-	const VERSION = '0.4.10';
+	const VERSION = '0.4.11';
 
 	//---------------------------------------------------------------------------------------------
 	// ~ Overriden static methods
