13c3c4ea061bf499bfd5197495c5f35bfdcd9587
Uwe Quitter <uwe.quitter@bestbytes.de>
2017-02-28 16:37:55 +0100

    version bumped to 0.5.4

--- lib/Foomo/Module.php
+++ lib/Foomo/Module.php
@@ -35,7 +35,7 @@ class Module extends \Foomo\Modules\ModuleBase implements \Foomo\Frontend\Toolbo
 	//---------------------------------------------------------------------------------------------
 
 	const NAME = 'Foomo';
-	const VERSION = '0.5.3';
+	const VERSION = '0.5.4';
 
 	//---------------------------------------------------------------------------------------------
 	// ~ Overriden static methods
